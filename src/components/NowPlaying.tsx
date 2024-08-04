cbfc9a28ebd37875dd4cd74645c83c5513ba630a
mboyers <mikeboyers@gmail.com>
2024-08-03 21:57:40 -0400

    made button trigger a refresh

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -10,29 +10,29 @@ const NowPlaying: React.FC = () => {
     const [song, setSong] = useState<string | null>(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('/api/nowPlaying/current');
-                console.log(response);
-                setStation(response.data.station);
-                setSong(response.data.song);
-            } catch (err) {
-                // setError('Failed to fetch data');
-            } finally {
-                // setLoading(false);
-            }
-        };
-
         fetchData();
     }, []); // Empty dependency array means this effect runs once on mount
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('/api/nowPlaying/current');
+            console.log(response);
+            setStation(response.data.station);
+            setSong(response.data.song);
+        } catch (err) {
+            // setError('Failed to fetch data');
+        } finally {
+            // setLoading(false);
+        }
+    };
 
     return (
         <Container>
             <Typography variant="h1" gutterBottom>
                 {station} {song}
             </Typography>
-            <Button variant="contained" color="primary">
-                Now Playing
+            <Button variant="contained" color="primary" onClick={fetchData}>
+                Refresh
             </Button>
         </Container>
     );
