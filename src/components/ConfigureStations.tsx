250317f2a147c3eec4b5875ffa3ff8e02a0e191c
mboyers <mikeboyers@gmail.com>
2024-08-10 19:53:13 -0400

    added a grid

--- src/components/ConfigureStations.tsx
+++ src/components/ConfigureStations.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import {Box, Button, TextField} from "@mui/material";
+import {Grid, TextField} from "@mui/material";
 import {useStationStore} from "../stores/stationStore";
 import PlayStationButton from "./PlayStationButton";
 import {useSnackbarStore} from "../stores/snackbarStore";
@@ -33,28 +33,33 @@ const ConfigureStations: React.FC = () => {
     };
 
     return (
-            <Box p={1}>
-                {stations.map((station, index) =>
-                    <React.Fragment key={station.dialPosition}>
-                        <Box display="flex" alignItems="center" m={1}>
-                            <PlayStationButton indexOfStation={index}/>
-                            <TextField
-                                label="Name"
-                                variant="outlined"
-                                value={station.name}
-                                onChange={(e) => handleNameChange(index, e.target.value)}
-                            />
-                            <TextField
-                                label="Streaming URL"
-                                variant="outlined"
-                                value={station.uri}
-                                onChange={(e) => handleUriChange(index, e.target.value)}
-                            />
-                        </Box>
-                    </React.Fragment>
-                )}
-                <Button variant="contained" color="primary" onClick={save}>Save</Button>
-            </Box>
+        <Grid container spacing={2}>
+            {stations.map((station, index) =>
+                <React.Fragment key={station.dialPosition}>
+                    <Grid item xs={12} sm={12} md={1}>
+                        <PlayStationButton indexOfStation={index}/>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                        <TextField
+                            label="Name"
+                            variant="outlined"
+                            value={station.name}
+                            fullWidth
+                            onChange={(e) => handleNameChange(index, e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={7}>
+                        <TextField
+                            label="Streaming URL"
+                            variant="outlined"
+                            value={station.uri}
+                            fullWidth
+                            onChange={(e) => handleUriChange(index, e.target.value)}
+                        />
+                    </Grid>
+                </React.Fragment>
+            )}
+        </Grid>
     );
 }
 
