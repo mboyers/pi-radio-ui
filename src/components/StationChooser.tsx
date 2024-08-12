558d255d72c6da604cbd7387aa8bcd16e349624a
mboyers <mikeboyers@gmail.com>
2024-08-11 23:40:05 -0400

    removed un-needed elements

--- src/components/StationChooser.tsx
+++ src/components/StationChooser.tsx
@@ -9,18 +9,14 @@ const StationChooser: React.FC = () => {
     const { stations} = useStationStore();
 
     return (
-        <div>
-            <Box p={1}>
-                    {stations.map((station, index) =>
-                        <React.Fragment key={station.dialPosition}>
-                            <Box display="flex" alignItems="center" m={1}>
-                                <PlayStationButton indexOfStation={index}/>
-                                <Typography variant="body1" style={{ paddingLeft: '20px' }}>{station.name}</Typography>
-                            </Box>
-                        </React.Fragment>
-                    )}
-            </Box>
-        </div>
+        <Box>
+            {stations.map((station, index) =>
+                <Box display="flex" alignItems="center" m={1}>
+                    <PlayStationButton indexOfStation={index}/>
+                    <Typography variant="body1" style={{ paddingLeft: '20px' }}>{station.name}</Typography>
+                </Box>
+            )}
+        </Box>
     );
 }
 
